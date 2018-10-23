aadf6ed637d05b251b858d49d58df9c215c85cc5
patelvishu05 <35935851+patelvishu05@users.noreply.github.com>
2018-10-22 19:00:49 -0500

    parseFile() method

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Sudoku
 {
     private int[][] table;
@@ -6,4 +10,31 @@ public class Sudoku
     {
         this.table = new int[8][8];
     }
+
+    public static void parseFile()
+    {
+        Scanner scan = null;
+        try
+        {
+             scan = new Scanner(new File("./sudoku.txt"));
+             while(scan.hasNextLine())
+             {
+                String line = scan.nextLine();
+                System.out.println(line);
+             }
+
+             scan.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if(scan!=null)
+                scan.close();
+        }
+    }   //end method finally
+
+
 }
