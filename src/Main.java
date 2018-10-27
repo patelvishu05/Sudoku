b0c206ea15e6c705826af3b8f49310d7ea82f4cb
patelvishu05 <35935851+patelvishu05@users.noreply.github.com>
2018-10-26 22:37:47 -0500

    additional functions

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Sudoku.parseFile();
+        Sudoku sudoku = new Sudoku();
+        sudoku.parseFile();
+        sudoku.printGrid();
+        System.out.print(sudoku.pickEmptyCells());
+
 
     }
 }
--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,29 +1,50 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
+//(i: Row(UP), j: Col(DOWN))
 public class Sudoku
 {
-    private int[][] table;
+    private static int[][] table;
+    private static ArrayList<String> emptyCells;
+
 
     Sudoku()
     {
-        this.table = new int[8][8];
+        table = new int[9][9];
+        emptyCells= new ArrayList<String>();
     }
 
-    public static void parseFile()
+    public int checkPlace()
+    {
+        return 0;
+    }
+
+    public String pickEmptyCells()
+    {
+        Collections.shuffle(emptyCells);
+        return emptyCells.get(emptyCells.size()-1);
+    }
+
+    public void parseFile()
     {
         Scanner scan = null;
         try
         {
-             scan = new Scanner(new File("./sudoku.txt"));
-             while(scan.hasNextLine())
-             {
-                String line = scan.nextLine();
-                System.out.println(line);
-             }
-
-             scan.close();
+            scan = new Scanner(new File("./grid1.txt"));
+            int i = 0;
+            while(scan.hasNextLine())
+            {
+                char[] tokens = scan.nextLine().toCharArray();
+                for(int j = 0; j < 9; j++ )
+                {
+                    table[i][j]=Character.getNumericValue(tokens[j]);
+                    if(table[i][j] == 0)
+                        emptyCells.add(""+i+j);
+                }
+                i++;
+            }
+            scan.close();
         }
         catch (FileNotFoundException e)
         {
@@ -34,7 +55,25 @@ public class Sudoku
             if(scan!=null)
                 scan.close();
         }
-    }   //end method finally
+    }   //end method parseFile
 
+    public void printGrid()
+    {
+        System.out.println("    0   1   2   3   4   5   6   7   8  ");
+        for(int i=0; i < 9; i++)
+        {
+            System.out.println("  +---+---+---+---+---+---+---+---+---+");
+            System.out.print(i);
+            for(int j=0; j < 9; j++)
+            {
+                if(table[i][j]==0)
+                    System.out.print(" |  ");
+                else
+                    System.out.print(" | " +table[i][j] );
+            }
+            System.out.print(" |\n");
+        }
+        System.out.println("  +---+---+---+---+---+---+---+---+---+");
+    }
 
-}
+}   //end class Sudoku
